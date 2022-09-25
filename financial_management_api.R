42488209ab814c48baac6d15e1a76ea26e4ff488
johnjmur0 <87945603+johnjmur0@users.noreply.github.com>
2022-09-25 15:31:49 -0400

    Ready for prod (#3)
    misc changes, nothing major

--- financial_management_api.R
+++ financial_management_api.R
@@ -2,31 +2,32 @@ library(dplyr)
 library(reticulate)
 
 # Command to manually launch api
-#pr('./financial_management_api.R') %>% pr_run(port=8000)
+# pr('./financial_management_api.R') %>% pr_run(port=8000)
 
-#* get historical transactions aggregated by month and category 
+#* get historical transactions aggregated by month and category
 #* @param user_name key for config file to use
 #* @param historical_start_year start year for data return
 #* @param time_vec time vector to aggregate on
 #* @param read_cache whether to read transactions from cache if possible
 #* @param write_cache whether to overwrite transactions in cache if not reading
 #* @post /get_historical_by_category
-get_historical_by_category = function(user_name, 
-                                      historical_start_year,
-                                      time_vec = c('year', 'month', 'day'), 
-                                      read_cache = FALSE, 
-                                      write_cache = FALSE) {
-  
-  config_list = utilities::get_user_config(user_name)
-  
-  transactions_df = mint.processor::get_mint_data_by_type_memoised('transactions', 
-                                                                   user_name, 
-                                                                   read_cache, 
-                                                                   write_cache)
-  
-  historical_start_date = utilities::create_datetime(historical_start_year, 1)
+get_historical_by_category <- function(user_name,
+                                       historical_start_year,
+                                       time_vec = c("year", "month", "day"),
+                                       read_cache = FALSE,
+                                       write_cache = FALSE) {
+  config_list <- utilities::get_user_config(user_name)
 
-  transactions_df %>% 
+  transactions_df <- mint.processor::get_mint_data_by_type_memoised(
+    "transactions",
+    user_name,
+    read_cache,
+    write_cache
+  )
+
+  historical_start_date <- utilities::create_datetime(historical_start_year, 1)
+
+  transactions_df %>%
     mint.processor::summarize_transactions(time_vec, config_list, historical_start_date)
 }
 
@@ -35,50 +36,56 @@ get_historical_by_category = function(user_name,
 #* @param read_cache whether to read accounts from cache if possible
 #* @param write_cache whether to overwrite transactions in cache if not reading
 #* @post /get_current_accounts
-get_current_accounts = function(user_name, 
-                                read_cache = FALSE, 
-                                write_cache = FALSE) {
-
-  accounts_df = mint.processor::get_mint_data_by_type_memoised('accounts', user_name, read_cache, write_cache)
+get_current_accounts <- function(user_name,
+                                 read_cache = FALSE,
+                                 write_cache = FALSE) {
+  accounts_df <- mint.processor::get_mint_data_by_type_memoised("accounts", user_name, read_cache, write_cache)
 
   accounts_df %>% mint.processor::clean_accounts_df()
 }
 
-#* get current monthly account projection 
+#* get current monthly account projection
 #* @param user_name key for config file to us
+#* @param historical_start_year start for historical data
 #* @param forecast_end_year last full year to be forecasted
 #* @param read_cache whether to read accounts from cache if possible
 #* @param write_cache whether to overwrite transactions in cache if not reading
 #* @post /get_current_projections
-get_current_projections = function(user_name, 
-                                  forecast_end_year,
-                                  time_vec = c('year', 'month'), 
-                                  read_cache = TRUE) {
-
-  config_list = utilities::get_user_config(user_name)
+get_current_projections <- function(user_name,
+                                    historical_start_year,
+                                    forecast_end_year,
+                                    time_vec = c("year", "month"),
+                                    read_cache = TRUE,
+                                    write_cache = TRUE) {
   
-  historical_transactions_df = get_historical_by_category(
-    user_name, 
-    time_vec, 
-    read_cache, 
+  config_list <- utilities::get_user_config(user_name)
+
+  historical_transactions_df <- get_historical_by_category(
+    user_name,
+    historical_start_year,
+    time_vec,
+    read_cache,
     write_cache
-  ) 
+  )
 
-  accounts_df = get_current_accounts(user_name, read_cache, write_cache)
-    
-  forecast_start = stringr::str_c(
-    lubridate::month(Sys.time()) + 1, 
-    1, 
-    lubridate::year(Sys.time()), 
-    sep = '/')
+  accounts_df <- get_current_accounts(user_name, read_cache, write_cache)
 
-  forecast_date_range = c(
-    lubridate::mdy(forecast_start), 
-    lubridate::mdy(stringr::str_c(12, 1, forecast_end_year, sep = '/')))
+  forecast_start <- stringr::str_c(
+    lubridate::month(Sys.time()) + 1,
+    1,
+    lubridate::year(Sys.time()),
+    sep = "/"
+  )
 
-  mint.processor::get_current_projections(
-    historical_transactions_df, 
-    accounts_df, 
-    config_list, 
-    forecast_date_range)
+  forecast_date_range <- c(
+    lubridate::mdy(forecast_start),
+    lubridate::mdy(stringr::str_c(12, 1, forecast_end_year, sep = "/"))
+  )
+
+  mint.processor::create_projections_df(
+    historical_transactions_df,
+    accounts_df,
+    config_list,
+    forecast_date_range
+  )
 }
--- mint_processor/R/analyze_mint_data.R
+++ mint_processor/R/analyze_mint_data.R
@@ -2,72 +2,65 @@
 #'
 #' @param transactions_df raw transactions
 #' @param time_vec vector to aggregate by
-#' @param config_list user config list 
+#' @param config_list user config list
 #' @param historical_start_date drop before this date
 #'
 #' @export
 #'
-summarize_transactions = function(transactions_df, time_vec, config_list, historical_start_date) {
-  
-  transactions_df %>% 
-
+summarize_transactions <- function(transactions_df, time_vec, config_list, historical_start_date) {
+  transactions_df %>%
     clean_transactions_df() %>%
-    
     summarise_categories(config_list, historical_start_date, time_vec, include_outlier = TRUE) %>%
-  
-    aggregate_categories(config_list[['aggregate_categories']], 'total', time_vec) %>%
-    aggregate_categories(config_list[['meta_categories']], 'total', time_vec)
+    aggregate_categories(config_list[["aggregate_categories"]], "total", time_vec) %>%
+    aggregate_categories(config_list[["meta_categories"]], "total", time_vec)
 }
 
-#' Get current financial account projections
+#' Create current financial account projections
 #'
 #' @param historical_transactions_df aggregated and cleaned transactions
 #' @param accounts_df aggregated and cleaned accounts
 #' @param config_list user config data
 #' @param forecast_date_range tuple of start and end date
 #'
+#'
 #' @export
 #'
-get_current_projections = function(historical_transactions_df, accounts_df, config_list, forecast_date_range) {
+create_projections_df <- function(historical_transactions_df, accounts_df, config_list, forecast_date_range) {
+  projection_df <- tibble("timestamp" = seq(min(forecast_date_range), max(forecast_date_range), by = "month"))
 
-  projection_df = tibble('timestamp' = seq(min(forecast_date_range), max(forecast_date_range), by = "month"))
+  starting_accounts_df <- accounts_df %>%
+    mutate("timestamp" = min(forecast_date_range)) %>%
+    dplyr::filter(total != 0)
 
-  starting_accounts_df = accounts_df %>% mutate('timestamp' = min(forecast_date_range)) %>% filter(total != 0)
-  
-  historical_avg_spend = historical_transactions_df %>% 
-    filter(category %in% c('discretionary', 'groceries')) %>%
-    group_by(year, month) %>% 
+  historical_avg_spend <- historical_transactions_df %>%
+    dplyr::filter(category %in% c("discretionary", "groceries")) %>%
+    group_by(year, month) %>%
     summarise(total = sum(total)) %>%
     ungroup() %>%
     summarise(total = mean(total)) %>%
     pull()
 
-  financial_config = config_list[['current_financial_params']]
+  financial_config <- config_list[["current_financial_params"]]
 
-  annual_income = financial_config[['current_salary']] * (1 - financial_config[['tax_rate']])
-  net_income = annual_income - financial_config[['contribution_401k']] - financial_config[['insurance']]
+  annual_income <- financial_config[["current_salary"]] * (1 - financial_config[["tax_rate"]])
+  net_income <- annual_income - financial_config[["contribution_401k"]] - financial_config[["insurance"]]
 
-  #TODO get bills from historical, need to add as category
-  projection_df %>% 
-    mutate(housing_bills = financial_config[['housing']] + financial_config[['bills']],
-           spend = historical_avg_spend + housing_bills,
-           
-           income = net_income / 12,
-           additional_savings = cumsum(spend + income),
-           
-           starting_savings = starting_accounts_df %>% filter(account_type == 'bank') %>% pull(total),
-           starting_investment = starting_accounts_df %>% filter(account_type == 'investment') %>% pull(total),
-           
-           total_savings = starting_savings + additional_savings,
-           
-           monthly_contribution = (financial_config[['contribution_401k']] + 
-                                    financial_config[['contribution_ira']]) / 12,
-
-           monthly_contribution = monthly_contribution + ((financial_config[['current_salary']] *
-                                                          financial_config[['employer_match']]) / 12),
-           
-           investment_growth = (1 + (financial_config[['annual_investment_growth']] / 12)) ^ row_number(),
-           total_investment = (starting_investment + cumsum(monthly_contribution)) ^ investment_growth,
-           net_worth = total_savings + total_investment,
-           )
- }
+  # TODO get bills from historical, need to add as category
+  projection_df %>%
+    mutate(
+      housing_bills = financial_config[["housing"]] + financial_config[["bills"]],
+      spend = historical_avg_spend + housing_bills,
+      income = net_income / 12,
+      additional_savings = cumsum(spend + income),
+      starting_savings = starting_accounts_df %>% dplyr::filter(account_type == "bank") %>% pull(total),
+      starting_investment = starting_accounts_df %>% dplyr::filter(account_type == "investment") %>% pull(total),
+      total_savings = starting_savings + additional_savings,
+      monthly_contribution = (financial_config[["contribution_401k"]] +
+        financial_config[["contribution_ira"]]) / 12,
+      monthly_contribution = monthly_contribution + ((financial_config[["current_salary"]] *
+        financial_config[["employer_match"]]) / 12),
+      investment_growth = (1 + (financial_config[["annual_investment_growth"]] / 12))^row_number(),
+      total_investment = (starting_investment + cumsum(monthly_contribution))^investment_growth,
+      net_worth = total_savings + total_investment,
+    )
+}
